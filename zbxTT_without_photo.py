d69cb4463608f54b1d307779a2da02ba8916fd54
vadikonline1 <vadikonline1@gmail.com>
2024-10-18 08:56:06 +0300

    Update zbxTT_without_photo.py
    Script parameters in Zabbix.UI

--- zbxTT_without_photo.py
+++ zbxTT_without_photo.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
-#python3 /usr/lib/zabbix/alertscripts/zbxTT.py "YOUR_CHAT_ID" "subject" "message"
+#python3 /usr/lib/zabbix/alertscripts/zbxTT.py "your_website" "your_api_token" "your_telegram_token" "YOUR_CHAT_ID" "subject" "message" "event_tags" "item_id"
 
 import json
 import requests
 import sys
 
+zabbix_url = sys.argv[1] if len(sys.argv) > 1 else '{$ZABBIX.URL}'
+zabbix_api_token = sys.argv[2] if len(sys.argv) > 2 else '{$ZABBIX_API_TOKEN}'
+zabbix_telegram_token = sys.argv[3] if len(sys.argv) > 3 else '{$ZABBIX_TELEGRAM_TOKEN}'
+chat_id = sys.argv[4] if len(sys.argv) > 4 else '{ALERT.SENDTO}'
+subject = sys.argv[5] if len(sys.argv) > 5 else '{ALERT.SUBJECT}'
+message = sys.argv[6] if len(sys.argv) > 6 else '{ALERT.MESSAGE}'
+event_tags = sys.argv[7] if len(sys.argv) > 7 else ''
+item_id = sys.argv[8] if len(sys.argv) > 8 else '{ITEM.ID}'
+message_thread_id_default = sys.argv[9] if len(sys.argv) > 9 else '1'  # The default value if the MessageThreadId tag is not found
+
+
 class TelegramNotifier:
-    def __init__(self, token, chat_id, parse_mode='markdownv2', proxy=None):
-        self.token = token
+    def __init__(self, zabbix_telegram_token, chat_id, parse_mode='markdownv2', proxy=None):
+        self.zabbix_telegram_token = zabbix_telegram_token
         self.chat_id = chat_id
         self.parse_mode = parse_mode
         self.proxy = proxy
-        self.message_thread_ids = []
+        self.message_thread_ids = ['106']
         self.message = ""
 
     def escape_markup(self, text):
@@ -52,7 +63,7 @@ class TelegramNotifier:
         if self.parse_mode:
             params['parse_mode'] = self.parse_mode
 
-        url = f'https://api.telegram.org/bot{self.token}/sendMessage'
+        url = f'https://api.telegram.org/bot{self.zabbix_telegram_token}/sendMessage'
 
         response = requests.post(url, json=params)
 
@@ -61,12 +72,6 @@ class TelegramNotifier:
 
 def main():
     try:
-        token = 'YOUR_BOT_TOKEN'  # Enter your token here
-        chat_id = sys.argv[1] if len(sys.argv) > 1 else '{ALERT.SENDTO}'
-        subject = sys.argv[2] if len(sys.argv) > 2 else '{ALERT.SUBJECT}'
-        message = sys.argv[3] if len(sys.argv) > 3 else '{ALERT.MESSAGE}'
-        event_tags = sys.argv[4] if len(sys.argv) > 4 else ''
-
         tags = {}
         if event_tags.strip():
             for tag in event_tags.split(','):
@@ -77,13 +82,13 @@ def main():
                     if key.startswith('MessageThreadId'):
                         tags.setdefault(key, []).append(value)
 
-        notifier = TelegramNotifier(token, chat_id)
+        notifier = TelegramNotifier(zabbix_telegram_token, chat_id)
         notifier.message = f"{subject}\n{message}"
 
         message_thread_ids = [val for key in tags for val in tags[key]]
 
         if not message_thread_ids:
-            message_thread_ids.append('1') # The default value if the MessageThreadId tag is not found
+            message_thread_ids.append(message_thread_id_default)
 
         notifier.message = notifier.escape_markup(notifier.message)
 
