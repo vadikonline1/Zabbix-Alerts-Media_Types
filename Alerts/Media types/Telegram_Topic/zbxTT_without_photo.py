94c886911a5782905c4b6531ee2e40a5e15e6bcc
vadikonline1 <vadikonline1@gmail.com>
2024-10-23 15:33:10 +0300

    Update zbxTT_without_photo.py
    check_chat_is_thread Method: This method checks whether the chat is a forum.
    Message Sending Logic: Depending on the result from check_chat_is_thread, the message is sent either with message_thread_id (if it's a forum) or as a regular message (if it's not).

--- Alerts/Media types/Telegram_Topic/zbxTT_without_photo.py
+++ Alerts/Media types/Telegram_Topic/zbxTT_without_photo.py
@@ -3,7 +3,7 @@
 #  https://t.me/vadikonline1 #
 ########################
 # https://github.com/vadikonline1/Zabbix-Repository
-#python3 /usr/lib/zabbix/alertscripts/zbxTT.py "your_telegram_token" "YOUR_CHAT_ID" "subject" "message" "event_tags" "item_id"
+# python3 /usr/lib/zabbix/alertscripts/zbxTT.py "your_telegram_token" "YOUR_CHAT_ID" "subject" "message" "event_tags" "item_id"
 
 import json
 import requests
@@ -15,16 +15,13 @@ subject = sys.argv[3] if len(sys.argv) > 3 else '{ALERT.SUBJECT}'
 message = sys.argv[4] if len(sys.argv) > 4 else '{ALERT.MESSAGE}'
 event_tags = sys.argv[5] if len(sys.argv) > 5 else ''
 item_id = sys.argv[6] if len(sys.argv) > 6 else '{ITEM.ID}'
-message_thread_id_default = sys.argv[7] if len(sys.argv) > 7 else '1'  # The default value if the MessageThreadId tag is not found
-
+message_thread_id_default = sys.argv[7] if len(sys.argv) > 7 else '1'  # Default value for message thread ID
 
 class TelegramNotifier:
-    def __init__(self, zabbix_telegram_token, chat_id, parse_mode='markdownv2', proxy=None):
+    def __init__(self, zabbix_telegram_token, chat_id, parse_mode='markdownv2'):
         self.zabbix_telegram_token = zabbix_telegram_token
         self.chat_id = chat_id
         self.parse_mode = parse_mode
-        self.proxy = proxy
-        self.message_thread_ids = []
         self.message = ""
 
     def escape_markup(self, text):
@@ -66,11 +63,20 @@ class TelegramNotifier:
             params['parse_mode'] = self.parse_mode
 
         url = f'https://api.telegram.org/bot{self.zabbix_telegram_token}/sendMessage'
-
         response = requests.post(url, json=params)
 
         if response.status_code != 200:
             raise Exception(f"Error sending message: {response.text}")
+
+    def check_chat_is_forum(self):
+        url = f'https://api.telegram.org/bot{self.zabbix_telegram_token}/getChat?chat_id={self.chat_id}'
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            raise Exception(f"Error getting chat info: {response.text}")
+
+        chat_info = response.json()
+        return chat_info.get('result', {}).get('is_forum', False)
 
 def main():
     try:
@@ -88,15 +94,22 @@ def main():
         notifier.message = f"{subject}\n{message}"
 
         message_thread_ids = [val for key in tags for val in tags[key]]
-
         if not message_thread_ids:
             message_thread_ids.append(message_thread_id_default)
 
         notifier.message = notifier.escape_markup(notifier.message)
 
+        # Check if the chat is a forum
+        is_forum = notifier.check_chat_is_forum()
+
         for thread_id in message_thread_ids:
             if thread_id:
-                notifier.send_message(message_thread_id=thread_id)
+                if is_forum:
+                    notifier.send_message(message_thread_id=thread_id)
+                else:
+                    # If not a forum, send the original message without thread_id
+                    notifier.message = f"{subject}\n{message}"
+                    notifier.send_message()
 
         return 'OK'
     except Exception as error:
